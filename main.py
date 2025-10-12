1ff4ac492256df09c10cb23a51f367f2a6806eb6
swdtkuy <tokai7asp3112@yahoo.co.jp>
2025-10-12 20:42:03 +0900

    move QUIT procedure to beginning of the loop

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
